9697290604e74b14abd4f4a730497394286ae3cb
Daniel R <danieo@Daniels-MacBook-Pro.local>
2022-06-26 10:35:43 -0300

    first commit

---
+++ src/components/SkillCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+
+export function SkillCard({skill}) {
+  return (
+    <TouchableOpacity key={skill} style={styles.buttonSkill}>
+      <Text style={styles.textSkill}>{skill}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonSkill: {
+    backgroundColor: '#1F1E25',
+    padding: 15,
+    borderRadius: 50,
+    marginVertical: 10,
+    alignItems: 'center',
+  },
+  textSkill: {
+    color: '#FFF',
+    alignItems: 'center',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+});
